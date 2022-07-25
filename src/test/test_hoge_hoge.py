97bbcea1ced4fa1c35dc5d11ff268336f58d91a2
okimototakuya <okimoto.takuya.sm@tut.jp>
2022-07-25 12:14:42 +0900

    プロダクト/テストコードと、ワークフローymlファイルをコミット - プロダクト/テストコード: 変数の出力と、そのテスト - ワークフローymlファイル: GitHub実践入門の内容を、一部コピペ

---
+++ src/test/test_hoge_hoge.py
@@ -0,0 +1,24 @@
+import unittest
+import sys
+sys.path.append('../main')
+import hoge_hoge
+
+class TestHogeHoge(unittest.TestCase):
+    '''
+    ../src/hoge_hogeモジュールをテスト
+    '''
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_(self):
+        '''
+        '''
+        x_1 = hoge_hoge.return_num()
+        x_2 = hoge_hoge.return_num()
+        self.assertEqual(x_1, x_2)
+
+if __name__ == '__main__':
+    unittest.main()
